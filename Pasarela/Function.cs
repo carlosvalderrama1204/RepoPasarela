d8d9ecb251df7d08a32daf75497c30e63564e4e7
Luis Carlos Valderrama <lvalderrama2@dxc.com>
2020-03-26 22:36:53 -0500

    httpclienteFactory

--- Pasarela/Function.cs
+++ Pasarela/Function.cs
@@ -1,18 +1,22 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Runtime.Serialization.Json;
-
 using Amazon.Lambda.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Polly;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
+using Microsoft.Graph;
 using System.Text;
+using Newtonsoft.Json;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+using static Pasarela.Function;
+using static Pasarela.Constants;
 using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.Model;
 using Amazon.DynamoDBv2.DocumentModel;
-using Amazon.DynamoDBv2.DataModel;
 
 
 
@@ -21,8 +25,9 @@ using Amazon.DynamoDBv2.DataModel;
 
 namespace Pasarela
 {
-    public static class Function
+    public class Function
     {
+        public IConfigurationService ConfigService { get; }
 
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
@@ -30,58 +35,35 @@ namespace Pasarela
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public static ResponseEcollect FunctionHandler(ILambdaContext context)
+        public async Task<TokenEcollect> FunctionHandler(string input, ILambdaContext context)
         {
-            
-            return ProcessRepositories();
-        }
-        private static ResponseEcollect ProcessRepositories()
-        {
-            ResponseEcollect respuestaEcollect;
 
-            try
+            var serviceCollection = new ServiceCollection();
+
+
+            serviceCollection.AddLogging(b =>
             {
-                string mensaje = JsonConvert.SerializeObject(
-                   new ContentEcollect
-                   {
-                       EntityCode = "41610",
-                       ApiKey = "4437637A572B766F4F587934664A62334E383577304D79657152584E77557331"
-                   });
-                StringContent content = new StringContent(mensaje, Encoding.UTF8, "application/json");
-
-                using (var client = new HttpClient())
-                {
+                b.AddFilter((category, level) => true); // Spam the world with logs.
 
 
-                    Task<HttpResponseMessage> res = client.PostAsync("https://test3.e-collect.com/app_express/api/getSessionToken"
-                      , content
-
-                    );
-
-                    LambdaLogger.Log(" va a consumir servicio1");
+            });
+            ConfigureServices(serviceCollection);
+            var services = serviceCollection.BuildServiceProvider();
 
 
-                    string respuesta = res.Result.Content.ReadAsStringAsync().Result;
-
-                    respuestaEcollect = JsonConvert.DeserializeObject<ResponseEcollect>(respuesta);
-
-                    Insertar(respuestaEcollect);
-                    return respuestaEcollect;
-
-                }
-
-            }
-            catch 
-            {
-                throw;
-               
-            }
+            var ecollectToken = services.GetRequiredService<EcollectTokenClient>();
 
 
+            var response = await ecollectToken.GetJson(ConfigService);
 
+            var data = await response.Content.ReadAsAsync<TokenEcollect>();
+            SaveToken(data);
+            LambdaLogger.Log("Comprobar variable: ");
+            //LambdaLogger.Log(ConfigService.GetConfiguration()[input] ?? "None");
+            return data;
         }
 
-        public static void Insertar(ResponseEcollect respuestaPasarela)
+        public static void SaveToken(TokenEcollect respuestaPasarela)
         {
             AmazonDynamoDBClient client = new AmazonDynamoDBClient();
             //var context = new DynamoDBContext(client);
@@ -90,8 +72,7 @@ namespace Pasarela
 
         }
 
-
-        private static async Task<string> PutDataAsync(Table table, ResponseEcollect respuestaPasarela)
+        private static async Task<string> PutDataAsync(Table table, TokenEcollect respuestaPasarela)
         {
             try
             {
@@ -99,38 +80,170 @@ namespace Pasarela
                 var doc = new Document
                 {
                     ["Id"] = Guid.NewGuid(),
-                    ["SessionToken"] = respuestaPasarela.SessionToken,  
-                    ["LifetimeSecs"] = respuestaPasarela.LifetimeSecs 
+                    ["SessionToken"] = respuestaPasarela.SessionToken,
+                    ["LifetimeSecs"] = DateTime.Now.AddSeconds(Convert.ToDouble(respuestaPasarela.LifetimeSecs))
                 };
 
                 Document x = await table.PutItemAsync(doc);
-         
+
                 return "success";
             }
-            catch 
+            catch
             {
                 throw;
             }
         }
-       
 
+        public Function()
+        {
+            // Set up Dependency Injection
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
 
+            // Get Configuration Service from DI system
+            ConfigService = serviceProvider.GetService<IConfigurationService>();
+        }
+
+        public interface IConfigurationService
+        {
+            IConfiguration GetConfiguration();
+        }
+
+        public interface IEnvironmentService
+        {
+            string EnvironmentName { get; set; }
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddTransient<IEnvironmentService, EnvironmentService>();
+            services.AddTransient<IConfigurationService, ConfigurationService>();
+
+            var registry = services.AddPolicyRegistry();
+
+            var timeout = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(10));
+            var longTimeout = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(30));
+
+            registry.Add("regular", timeout);
+            registry.Add("long", longTimeout);
+
+            services.AddHttpClient("ecollectToken", c =>
+            {
+
+            })
+
+            // Build a totally custom policy using any criteria
+            .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(10)))
+
+            // Use a specific named policy from the registry. Simplest way, policy is cached for the
+            // lifetime of the handler.
+            .AddPolicyHandlerFromRegistry("regular")
+
+            // Run some code to select a policy based on the request
+            .AddPolicyHandler((request) =>
+            {
+                return request.Method == HttpMethod.Get ? timeout : longTimeout;
+            })
+
+            // Run some code to select a policy from the registry based on the request
+            .AddPolicyHandlerFromRegistry((reg, request) =>
+            {
+                return request.Method == HttpMethod.Get ?
+                    reg.Get<IAsyncPolicy<HttpResponseMessage>>("regular") :
+                    reg.Get<IAsyncPolicy<HttpResponseMessage>>("long");
+            })
+
+            // Build a policy that will handle exceptions, 408s, and 500s from the remote server
+            .AddTransientHttpErrorPolicy(p => p.RetryAsync())
+
+            .AddHttpMessageHandler(() => new RetryHandler()) // Retry requests to github using our retry handler
+            .AddTypedClient<EcollectTokenClient>();
+        }
     }
 
 
+    public class EcollectTokenClient
+    {
+        public EcollectTokenClient(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
 
+        public HttpClient HttpClient { get; }
 
-    public class ContentEcollect
+        // Gets the list of services on github.
+        public async Task<HttpResponseMessage> GetJson(IConfigurationService ConfigService)
+        {
+            string mensaje = JsonConvert.SerializeObject(
+                    new ContentTokenEcollect
+                    {
+                        EntityCode = ConfigService.GetConfiguration()["EntityCode"],
+                        ApiKey = ConfigService.GetConfiguration()["ApiKey"]
+                    });
+            StringContent content = new StringContent(mensaje, Encoding.UTF8, "application/json");
+            var response = await HttpClient.PostAsync(ConfigService.GetConfiguration()["uri"], content).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+
+            return response;
+        }
+    }
+
+    public class ContentTokenEcollect
     {
         public string ApiKey { get; set; }
         public string EntityCode { get; set; }
     }
-    public class ResponseEcollect
+
+    public class TokenEcollect
     {
-        public string SessionToken;
-        public string LifetimeSecs;
-        public string ReturnCode;
+        public string SessionToken { get; set; }
+        public string LifetimeSecs { get; set; }
+        public string ReturnCode { get; set; }
     }
 
-}
+    public class EnvironmentService : IEnvironmentService
+    {
+        public EnvironmentService()
+        {
+            EnvironmentName = Environment.GetEnvironmentVariable(EnvironmentVariables.AspnetCoreEnvironment)
+                ?? Environments.Production;
+        }
 
+        public string EnvironmentName { get; set; }
+    }
+
+    public static class Constants
+    {
+        public static class EnvironmentVariables
+        {
+            public const string AspnetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+        }
+
+        public static class Environments
+        {
+            public const string Production = "Production";
+        }
+    }
+
+    public class ConfigurationService : IConfigurationService
+    {
+        public IEnvironmentService EnvService { get; }
+
+        public ConfigurationService(IEnvironmentService envService)
+        {
+            EnvService = envService;
+        }
+
+        public IConfiguration GetConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{EnvService.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+    }
+}
